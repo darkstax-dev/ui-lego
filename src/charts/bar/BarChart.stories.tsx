5090827267a3c80e0ae8ea0da03b215ab3251184
rmulaveesala <roni.mulaveesala@ad-hocresearch.com>
2025-09-25 11:36:59 -0500

    fix ci-cd errors pt 4

--- src/charts/bar/BarChart.stories.tsx
+++ src/charts/bar/BarChart.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import BarChart, { GroupedBarChartData } from './BarChart';
 import BarChartDemo from './BarChartDemo';
 
--- src/charts/stream/StreamChart.stories.tsx
+++ src/charts/stream/StreamChart.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { StreamChart } from './StreamChart';
 import StreamChartDemo from './StreamChartDemo';
 
