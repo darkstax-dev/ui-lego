531b20c8835ec8b6ec6e1696cf94f036d16f10b2
rmulaveesala <roni.mulaveesala@ad-hocresearch.com>
2025-09-25 11:39:41 -0500

    fix ci-cd errors pt 5

--- src/components/icons/logo/Logo.stories.tsx
+++ src/components/icons/logo/Logo.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import LogoTile from './LogoTile'
 import NexoraWordmarkWhite from './NexoraWordmarkWhite'
 import './LogoCatalog.css'
